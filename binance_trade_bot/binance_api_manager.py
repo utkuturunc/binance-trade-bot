8926e2c7d2ca3a86dbf0b0bf09a037a9d7d62bec
Utku Turunç <utku@turunc.me>
2021-05-13 14:19:49 +0200

    Fix fee

--- binance_trade_bot/binance_api_manager.py
+++ binance_trade_bot/binance_api_manager.py
@@ -42,7 +42,7 @@ class BinanceAPIManager:
         return self.binance_client.get_bnb_burn_spot_margin()["spotBNBBurn"]
 
     def get_fee(self, origin_coin: Coin, target_coin: Coin, selling: bool):
-        return 0.0001
+        return 0.001
         # base_fee = self.get_trade_fees()[origin_coin + target_coin]
         # if not self.get_using_bnb_for_fees():
         #     return base_fee
